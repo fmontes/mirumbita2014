9ac1a5330d835309fb50d82a90026b64eb592e3d
Freddy Montes <freddymontes@gmail.com>
2014-01-05 17:49:42 -0600

    No user scalable viewport

--- app/includes/head_meta.php
+++ app/includes/head_meta.php
@@ -12,7 +12,7 @@
         <?php printHeadTitle(); ?>
         <meta charset="<?php echo LOCAL_CHARSET; ?>">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <meta name="viewport" content="width=device-width">
+        <meta name="viewport" content="width=device-width,user-scalable=no">
         <meta name="description" content="">
         <link rel="stylesheet" href="<?php echo $_zp_themeroot; ?>/css/main.css" type="text/css" />
 
