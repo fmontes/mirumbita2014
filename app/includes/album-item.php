fccfcfb6aaa3ff2dda6bcb761235d3ce0b4f4cf7
Freddy Montes <freddymontes@gmail.com>
2014-01-05 21:04:41 -0600

    Fix the thumbnails in the album pages

--- app/includes/album-item.php
+++ app/includes/album-item.php
@@ -1,6 +1,8 @@
 <li>
     <article>
-        <a href="<?php echo html_encode(getAlbumLinkURL());?>" title="<?php echo gettext("View album:"); ?> <?php echo getAnnotatedAlbumTitle();?>"><?php printAlbumThumbImage(getAnnotatedAlbumTitle()); ?></a>
+        <a href="<?php echo html_encode(getAlbumLinkURL());?>" title="<?php echo gettext("View album:"); ?> <?php echo getAnnotatedAlbumTitle();?>">
+            <img src="<?php echo getAlbumThumb(); ?>" alt="<?php echo getAnnotatedAlbumTitle();?>">
+        </a>
         <h3><a href="<?php echo html_encode(getAlbumLinkURL());?>" title="<?php echo gettext("View album:"); ?> <?php echo getAnnotatedAlbumTitle();?>"><?php printAlbumTitle(); ?></a></h3>
         <time><?php printAlbumDate(""); ?></time>
     </article>
