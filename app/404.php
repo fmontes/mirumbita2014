027b92822e4c484561d76b68425e779ca48f11c1
Freddy Montes <freddymontes@gmail.com>
2014-01-22 20:57:19 -0600

    404 page work done

--- app/404.php
+++ app/404.php
@@ -15,8 +15,23 @@
 
                 <div class="content-wrapper">
                     <div class="content" role="main">
-                        <h1 class="page-title"><?php echo gettext("Party is not here"); ?></h1>
-                        <p><?php echo gettext("Error 404: Page not found"); ?></p>
+                        <h1 class="page-title"><?php echo gettext_th("Party is not here", "mirumbita"); ?></h1>
+                        <p><?php echo gettext_th("Error 404: Page not found", "mirumbita"); ?></p>
+                        <h4>Puedes realizar una busqueda:</h4>
+                        <div class="search">
+                            <?php printSearchForm("", "header-search", "", gettext("Search"), "", [title, desc], [], false); ?>
+                        </div>
+                        <h4>Categorías</h4>
+                        <ul class="categories">
+                            <li><a href="/index.php?album=Baby-Showers" title="Baby Showers">Baby Showers</a></li>
+                            <li><a href="/index.php?album=Bautizos" title="Bautizos">Bautizos</a></li>
+                            <li><a href="/index.php?album=Comuniones" title="Comuniones">Comuniones</a></li>
+                            <li><a href="/index.php?album=Cumpleanos" title="Cumpleaños">Cumpleaños</a></li>
+                            <li><a href="/index.php?album=Escolares" title="Escolares">Escolares</a></li>
+                            <li><a href="/index.php?album=Especiales" title="Especiales">Especiales</a></li>
+                            <li><a href="/index.php?album=Nacimientos" title="Nacimientos">Nacimientos</a></li>
+                        </ul>
+
                     </div><!-- // content -->
                 </div><!-- // content-wrapper -->
 
