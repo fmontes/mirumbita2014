8a3dfc8c25b4764f30d2671def31a5b2066880e5
Freddy Montes <freddymontes@gmail.com>
2014-01-22 21:49:20 -0600

    Add copyright to the footer

--- app/includes/footer.php
+++ app/includes/footer.php
@@ -1,5 +1,6 @@
 <div class="footer-wrapper">
     <footer class="footer">
-        <?php zp_apply_filter("theme_body_close");?>
+        <p class="copyright"><?php echo gettext_th("Copyright© " . date('Y') . ". All rights reserved", "mirumbita"); ?></p>
     </footer>
 </div><!-- // footer-wrapper -->
+<?php zp_apply_filter("theme_body_close");?>
