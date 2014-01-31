c811342184a226ab47ba7f1126848a74e874076e
Freddy Montes <freddymontes@gmail.com>
2014-01-30 22:03:25 -0600

    Truncate title in the album page

--- app/includes/album-item.php
+++ app/includes/album-item.php
@@ -4,7 +4,15 @@
             <img src="<?php echo getCustomAlbumThumb(80,80,80,80); ?>" alt="<?php echo getAnnotatedAlbumTitle();?>">
         </a>
         <div class="album-info">
-            <h3 class="album-title"><a href="<?php echo html_encode(getAlbumLinkURL());?>" title="<?php echo gettext("View album:"); ?> <?php echo getAnnotatedAlbumTitle();?>"><?php printAlbumTitle(); ?></a></h3>
+            <h3 class="album-title">
+                <a href="<?php echo html_encode(getAlbumLinkURL());?>" title="<?php echo gettext("View album:"); ?> <?php echo getAnnotatedAlbumTitle();?>">
+                    <?php
+                        $temp = getAlbumTitle();
+                        $title = (strlen($temp) > 40) ? substr($temp, 0, 40).'…' : $temp;
+                        echo $title;
+                    ?>
+                </a>
+            </h3>
             <div class="album-meta">
                 <time datetime="<?php printAlbumDate(); ?>" class="album-date"><?php printAlbumDate(); ?></time>
                 <span class="album-image-number"><?php echo getNumImages() . " " . gettext("photos") ?></span>
